2ecd488011e3a6e449c82e2607d41fa4da78cbb2
SoloMessiah <ch1n0ul33@gmail.com>
2021-05-12 22:14:07 -0700

    Finished code.

--- lab_6.py
+++ lab_6.py
@@ -1,4 +1,4 @@
- """
+"""
 We will create a random number between one and twenty and based on the results of the random
 number, we will check to see if the number falls within certain ranges.
 If the number is greater than 15, the result will be "Cherries".
@@ -9,11 +9,10 @@ If the number is greater than 1, the result will be "Bell".
 If the number is not any of the above, than the result will be "Bar".
 
 We loop three times and print the results to the user.
- """
+"""
 
 
-
- """
+"""
  import random module
 num = generated random number
 
@@ -32,4 +31,34 @@ Else
 
 loop three times
     print the output(fruit)
- """
+"""
+
+
+
+
+import random
+
+def main():
+    for i in range(0, 3):
+        spin()
+
+def spin():
+    rand_num = random.randint(1, 20)
+    output = ""
+
+    if(rand_num > 15):
+        output = "Cherries"
+    elif(rand_num > 10):
+        output = "Orange"
+    elif(rand_num > 5):
+        output = "Plum"
+    elif(rand_num > 2):
+        output = "Melon"
+    elif(rand_num > 1):
+        output = "Bell"
+    else:
+        output = "Bar"
+
+    print(output, end=" ")
+
+main()
